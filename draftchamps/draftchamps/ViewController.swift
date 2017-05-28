c2840bca9e78f0bdb9a3c3537805ded5b095de8d
Wilson Wang <wilsonw926@gmail.com>
2017-05-27 21:14:26 -0700

    empty xcode proj

---
+++ draftchamps/draftchamps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  draftchamps
+//
+//  Created by Wilson Wang on 5/27/17.
+//  Copyright © 2017 Wilson Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
