4a8cb3cc5fe9dd4601e290957d82b035313d017e
Wilson Wang <wilsonw926@gmail.com>
2017-08-12 16:21:54 -0700

    added options to view offense/defense and started player class

---
+++ draftchamps/Player.swift
@@ -0,0 +1,22 @@
+//
+//  Player.swift
+//  draftchamps
+//
+//  Created by Wilson Wang on 8/27/17.
+//  Copyright © 2017 Wilson Wang. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class Snap {
+    
+    var overall: Int?
+    var playerPic: UIImage?
+    var statsPic: UIImage?
+    var name: String = ""
+    var position: Int?
+    
+}
+
