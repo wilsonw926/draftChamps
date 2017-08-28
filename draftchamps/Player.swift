3afbe849dc40ea726b238a5542d8f63a3f2a9756
Wilson Wang <wilsonw926@gmail.com>
2017-08-28 10:23:38 -0700

    update repo

--- draftchamps/Player.swift
+++ draftchamps/Player.swift
@@ -17,13 +17,15 @@ class Player {
     var statsPic: UIImage?
     var name: String = ""
     var position: Int?
+    var randomID: Int?
     
-    init(imageToPost frontimage: UIImage, backimage: UIImage, name: String, position: Int, overall: Int) {
+    init(imageToPost frontimage: UIImage, backimage: UIImage, name: String, position: Int, overall: Int, randomID: Int) {
         self.playerPic = frontimage
         self.statsPic = backimage
         self.name = name
         self.position = position
         self.overall = overall
+        self.randomID = randomID
     }
     
 }
